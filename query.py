c149e73c5891a3a6d13b705a614e1e9085477e1f
Giuseppe Greco <giuseppe.greco@uniurb.it>
2018-10-19 14:12:45 +0200

    Add files via upload

---
+++ query.py
@@ -0,0 +1,44 @@
+
+#from astropy.coordinates import SkyCoord
+from astroquery.vizier import Vizier
+from astropy import units as u
+
+#from astroquery.vizier import Vizier
+#import astropy.units as u
+import astropy.coordinates as coord
+
+class Query(object):
+    """Galaxy query inside a user-defined FoV."""
+
+    def query_box(self, ra, dec, base, height, catalog, column_1, filter_1):
+        """Vizier.query_region in a square/rectangular FoV using astroquery module."""
+
+        base_str, height_str = str(base) + 'd', str(height) + 'd'
+        
+        catalog_constraints = Vizier(catalog=[catalog],
+                                     columns=['*', '_RAJ2000', '_DEJ2000', column_1],
+                                     column_filters={column_1 : filter_1})
+        
+        catalog_constraints.ROW_LIMIT = -1  # completed catalog
+        query_result = catalog_constraints.query_region(coord.SkyCoord(
+            ra = ra, dec = dec, unit = (u.deg, u.deg), frame='icrs'),
+                                                        width = height_str, height = base_str)       
+        return query_result
+
+    def query_circle(self, ra, dec, radius, catalog, column_1, filter_1):
+        """Vizier.query_region in a circle FoV  using astroquery module."""
+        
+        radius_str = str(radius) + 'd'
+        
+        catalog_constraints = Vizier(catalog=[catalog],
+                                     columns=['*', 'RAJ2000', 'DEJ2000', column_1],
+                                     column_filters={column_1 : filter_1})
+        
+        catalog_constraints.ROW_LIMIT = -1  # completed catalog
+        query_result = catalog_constraints.query_region(coord.SkyCoord(
+            ra = ra, dec = dec, unit = (u.deg, u.deg), frame='icrs'), radius = radius_str)
+        
+        return query_result
+
+
+c=Query()
